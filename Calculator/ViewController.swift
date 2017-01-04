df9aeb270069976f4ce6ae126f1dcd17369e4324
pkmurphy85 <pkmurphy85@gmail.com>
2017-01-04 15:23:41 -0500

    Adding calculator demonstration thru lecture 3

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Patrick Murphy on 1/3/17.
+//  Copyright © 2017 Patrick Murphy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet private weak var display: UILabel!
+    
+    private var userIsInTheMiddleOfTyping = false
+    
+    @IBAction private func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        print(userIsInTheMiddleOfTyping)
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+    }
+    
+    // computed property, tracks display
+    private var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    var savedProgram: CalculatorBrain.PropertyList?
+    
+    @IBAction func save() {
+        savedProgram = brain.program
+    }
+    
+    @IBAction func restore() {
+        if savedProgram != nil {
+            brain.program = savedProgram!
+            displayValue = brain.result
+        }
+    }
+    
+    
+    // every new class has one free initializer which is an initializer that takes no arguments. That is used here
+    private var brain = CalculatorBrain()
+    
+    @IBAction private func performOperation(sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        if let mathematicalSymbol = sender.currentTitle{
+            brain.performOperation(mathematicalSymbol)
+        }
+        displayValue = brain.result
+    }
+}
+
